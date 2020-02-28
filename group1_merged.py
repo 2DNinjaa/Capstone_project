f547b2d3090c31db69fb3fbbcf8869a4d0e0e232
enrique343 <enriqueg0604@gmail.com>
2020-02-28 15:26:24 -0600

    Cleaned up the file

--- group1_merged.py
+++ group1_merged.py
@@ -153,11 +153,6 @@ class data:
         cursor.close()
         conn.close()
     
-    def getOwnListing(self):
-        return self.listing
-    
-
-
 
 
 
@@ -166,19 +161,7 @@ class data:
 
 ###CAN CHANGE TO RUN LINKS THROUGH METHODS (have links as method parameters)
 
-    #prints links of positions by location (city or state)
-    def getLocationLinks(self,local):
-        loc = local.replace(' ', '+')
-        url = 'https://jobs.github.com/positions?utf8=%E2%9C%93&description=&location=' + loc
-        source = requests.get(url).text
-        soup = BeautifulSoup(source, 'lxml')
-
-        locationLinks = []
-        for link in soup.find_all('a'):
-            links = link.get('href')
-            if "http" in links and '/positions/' in links:
-                locationLinks.append(links)
-        return locationLinks
+    
 
 
     #returns list of links of positions without location specification 
@@ -196,143 +179,7 @@ class data:
         return linklist
 
 
-    #prints links for positions through page x
-    def linksMultPages(self,x):
-        for i in range (1,x):
-            url = 'https://jobs.github.com/positions'
-            finalurl = url + "?page=" + str(i)
-            source = requests.get(url).text
-            soup = BeautifulSoup(source, 'lxml')
-            
-            for link in soup.find_all('a'):
-                links = link.get('href')
-                if "http" in links and '/positions/' in links:
-                    print (links) # TODO: replace with return?
 
-
-    #prints links for positions through page x in location loc
-    #for this website there usually isnt multiple pages so this might not be necessary.
-    def linksMultPagesLoc(self,x, loc):
-        url = 'https://jobs.github.com/positions'
-        loc = loc.replace(' ', '+')
-        for i in range (1,x):
-            finalurl = url + "?page=" + str(i) + loc
-            source = requests.get(url).text # TODO: should this say finalurl not url?
-            soup = BeautifulSoup(source, 'lxml')
-            
-            for link in soup.find_all('a'):
-                links = link.get('href')
-                if "http" in links and '/positions/' in links:
-                    print (links) # TODO: replace with return?
-
-
-
-    #prints out all job titles, locations, company, and fulltime or not full time
-    def PositionList(self):
-        source = requests.get('https://jobs.github.com/positions').text
-        soup = BeautifulSoup(source, 'lxml')
-
-        for pos in soup.find_all('table', class_='positionlist'):
-            desc = pos.text
-            print (desc) # TODO: replace with return?
-
-
-    #returns list of the titles of all the positions on the page    
-    def positionNamesOnly(self):
-        pnames = []
-        source = requests.get('https://jobs.github.com/positions').text
-        soup = BeautifulSoup(source, 'lxml')
-
-        for pos in soup.find_all('tr', class_='job'):
-            name = pos.td.h4.text
-            #print (name)
-            pnames.append(name)
-        return (pnames)
-
-
-    #returns list of the company names 
-    def company(self):
-        cos = []
-        source = requests.get('https://jobs.github.com/positions').text
-        soup = BeautifulSoup(source, 'lxml')
-
-        for co in soup.find_all('a', class_='company'):
-            coname = co.text
-            #print (coname)
-            cos.append(coname)
-            
-        return (cos)
-
-
-    #returns list of the locations
-    def getLocation(self):
-        locs = []
-        source = requests.get('https://jobs.github.com/positions').text
-        soup = BeautifulSoup(source, 'lxml')
-
-        for pos in soup.find_all('span', class_='location'): #td, meta
-            loc = pos.text
-            #print (loc)
-            locs.append(loc)
-        return (locs)
-
-    #returns list of the times created
-    def timeCreated(self):
-        tcr = []
-        source = requests.get('https://jobs.github.com/positions').text
-        soup = BeautifulSoup(source, 'lxml')
-
-        for pos in soup.find_all('span', class_='when relatize relatized'):
-            time = pos.text
-            tcr.append(time)
-        return (tcr)
-
-    #returns list of full/parttime
-    def FullTime(self):
-        ft = []
-        source = requests.get('https://jobs.github.com/positions').text
-        soup = BeautifulSoup(source, 'lxml')
-
-        for pos in soup.find_all('strong', class_='fulltime'):
-            time = pos.text
-            ft.append(time)
-        return (ft)
-        
-    #returns list of the skills found (as part of the preset skill list) and creates a list for those skills found
-    def getSkills(self,link):
-        #can add more keywords
-        #keyWordSkills = ['python', 'java', 'C++', 'SQL', 'manage', 'javascript', 'linux', 'team', 'problem solving', 'front end', 'back end']
-        foundSkillsList = []
-        
-        url = link
-        src = requests.get(url).text
-        soup = BeautifulSoup(src, 'lxml')
-
-        summary = soup.find('div', class_='column main')
-        #print (summary)
-        sumtext = summary.text
-        sumList = sumtext.split()
-        #print ('====================================')
-
-        for i in self.keyWordSkills:
-            if i in sumList:
-                foundSkillsList.append(i)
-                #print('found skill!!  ' + i)
-                
-        return foundSkillsList
-
-
-    #goes through multiple pages using getSkillS
-    def findallSkills(self):
-        urls = []
-        #30 just sample
-        for i in range (1,30):
-            url ='https://jobs.github.com/positions'
-            finalurl = url + "?page=" + str(i)
-            urls.append(finalurl)
-
-        for x in urls:  #add print?
-            getSkills(x)
             
     # searches multiple pages and the given location
     # returns list of all jobs found
@@ -387,45 +234,6 @@ class data:
             print("Page: "+str(i))
         return multPageLocJobsWL
             
-    #makes a dictionary of the return of the url loc,co,time,title,type,skills (some dictionaries do not have skills if job didnt have any of the keyword skills)
-    #runs on the first page only (no location specified) (doesnt go through multiple pages) url used ='https://jobs.github.com/positions'
-    #the try/excepts are there because some jobs do not have those fields specified in a normal way
-    def makeDict(self,x):
-        Dict = {}
-        companyList = company()
-        locList = location()
-        TitleList = positionNamesOnly()
-        times = []
-        try:
-            times = timeCreated()
-        except:
-            pass
-            
-        full = FullTime()
-        links = onlylinks()
-        jobSkills = []
-        
-        try:
-            jobSkills = getSkills(links[x])
-            #print (jobSkills)
-        except:
-            pass
-        
-        #print (locList)
-        
-        try:
-            Dict = dict({'Company': companyList[x], 'Location': locList[x], 'Title': TitleList[x], 'Time Created': times[x], 'Type': full[x]})
-        except:
-            pass
-
-        try: #it is only adding first skill found, not sure why --->this might be messing it up actually some returns only give skills
-            if (len(jobSkills) > 0):
-                Dict.update({'Skills': jobSkills})
-        except:
-            pass
-
-        return (Dict)
-
 
     # TODO
     #going to be a list of dictionaries ***TBA
@@ -435,31 +243,8 @@ class data:
         self.jobLst = self.getJobLLP (loc, numPages)
         self.testing()
         print("create done")
-        #source = requests.get('https://jobs.github.com/positions').text
-        #soup = BeautifulSoup(source, 'lxml')
-        
-        #the length of the list of jobs
-        #length = len(soup.find_all('tr', {'class':'job'} ))
-        #for i in range (0, length):
-        #for job in soup.find_all('tr', {'class':'job'} ):
-            #source = requests.get('https://jobs.github.com/positions').text
-            #soup = BeautifulSoup(source, 'lxml')
-            #print (makeDict(job))
             
        
-
-
-    # helper function for getJobWLnks
-    # assumes url is a job page on the github jobs api
-    # returns the link where the user can apply to the company
-    # or this page (as specified by url) if none provided
-    # do not use, redundant
-    def getLink(self,url):
-        newSrc = requests.get(url).text
-        newSoup = BeautifulSoup(newSrc, 'lxml')
-        jobLink = newSoup.find('div', {'class':'highlighted'}).find('a')
-        return url if jobLink == None else jobLink['href']
-
 
     # returns 4 pieces of the specified page
     #   [string:applyTo_link, list:skills, string:description, string:page_url]
